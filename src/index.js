94c2127a044fcffdfcaf6661445e8b23261cd8f7
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2023-01-03 23:25:55 +0300

    Add dependencies

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
